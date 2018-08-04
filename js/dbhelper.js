824a429fd43720207b266de84f392ef5109e5d0c
Mike Meyerson <michael.b.meyerson@aexp.com>
2018-08-03 22:04:51 -0400

    Image fallback

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -96,7 +96,11 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}.jpg`);
+    const fileName = restaurant.photograph
+      ? `${restaurant.photograph}.jpg`
+      : 'not-found.svg';
+
+    return (`/img/${fileName}`);
   }
 
   /**
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 4;
+const version = 5;
 const currentCacheName = `restaurants-cache-v${version}`;
 
 self.addEventListener('install', event => {
