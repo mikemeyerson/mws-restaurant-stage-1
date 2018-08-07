db85687dbbb5aeeb6bbd27749beedef43fc5cfdf
Mike Meyerson <michael.b.meyerson@aexp.com>
2018-08-06 20:13:41 -0400

    Skip bundle & API request for cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 2;
+const version = 5;
 const currentCacheName = `restaurants-cache-v${version}`;
 
 self.addEventListener('install', (event) => {
@@ -16,17 +16,30 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.open(currentCacheName).then(cache => cache.match(event.request).then((cacheResponse) => {
-      if (cacheResponse) {
-        return cacheResponse;
-      }
+  const requestUrl = new URL(event.request.url);
+  // TODO: Remove me
+  const isJSFile = requestUrl.pathname.startsWith('/dist');
+  const isAPI = requestUrl.hostname === location.hostname && requestUrl.port === '1337';
 
-      return fetch(event.request).then((fetchResponse) => {
-        cache.put(event.request, fetchResponse.clone());
-        return fetchResponse;
-      });
-    }))
+  if (isJSFile || isAPI) {
+    event.respondWith(
+      fetch(event.request),
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.open(currentCacheName)
+      .then(cache => cache.match(event.request).then((cacheResponse) => {
+        if (cacheResponse) {
+          return cacheResponse;
+        }
+
+        return fetch(event.request).then((fetchResponse) => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      }))
       .catch((err) => {
         console.error('Failed on SW fetch');
         console.error(err);
